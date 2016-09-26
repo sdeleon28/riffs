27656d25b715761f3ce22b0ba84e64eded0c4e83
Santiago de Leon <sdeleon28@gmail.com>
2016-09-26 15:40:27 -0300

    Implemented logic to gather file/folder metadata

---
+++ app/files/gatherMetadata.js
@@ -0,0 +1,45 @@
+import fs from 'fs';
+import _ from 'lodash';
+import ini from 'ini';
+
+
+export const gatherMetadata = dir => {
+  const ideaDirs = fs.readdirSync(dir);
+  return ideaDirs.map(ideaDirName => {
+    const ideaDir = `${dir}/${ideaDirName}`;
+    const stat = fs.statSync(ideaDir);
+    if (stat && stat.isDirectory()) {
+      const files = fs.readdirSync(ideaDir);
+      let meta = {
+        name: ideaDirName,
+        keys: [],
+        tags: [],
+      };
+      if (_.includes(files, 'meta.ini')) {
+        meta = parseMeta(`${ideaDir}/meta.ini`);
+      }
+      return {
+        ...meta,
+        ...categorizeFiles(files.map(x => `${ideaDir}/${x}`)),
+      };
+    } else {
+      return null;
+    }
+  }).filter(x => x != null);
+}
+
+const categorizeFiles = files => ({
+  audio: files.filter(x => x.endsWith('.mp3')),
+  score: files.find(x => x.endsWith('/score.jpg')) || null,
+});
+
+const parseMeta = filename => {
+  const iniObj = parseIni(filename);
+  return {
+    ...iniObj,
+    keys: iniObj.keys ? iniObj.keys.split(' ') : [],
+    tags: iniObj.tags ? iniObj.tags.split(' ') : [],
+  };
+};
+
+const parseIni = filename => ini.decode(fs.readFileSync(filename, 'utf8'));
