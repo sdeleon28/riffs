1d78480ba3400381a77ee255b11371e100dc17d3
Santiago de Leon <sdeleon28@gmail.com>
2016-09-27 10:59:11 -0300

    Implemented rehydration from local configs

--- app/index.js
+++ app/index.js
@@ -3,11 +3,15 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { Router, hashHistory } from 'react-router';
 import { syncHistoryWithStore } from 'react-router-redux';
+import { getDirectory } from './files/config';
+import gatherMetadata from './files/gatherMetadata';
 import routes from './routes';
 import configureStore from './store/configureStore';
 import './app.global.css';
 
-const store = configureStore();
+const directory = getDirectory();
+const metadata = gatherMetadata(directory);
+const store = configureStore({ directory, metadata });
 const history = syncHistoryWithStore(hashHistory, store);
 
 render(
